9ecc5e6a9eb7e0e1c6ff5b5cbd38d984cdf33d8e
krasi070 <krasi0707@gmail.com>
2017-12-19 23:52:54 +0200

    fixed login bug

--- SpaceshipClicker/SpaceshipClicker.Web/Models/AccountViewModels/LoginViewModel.cs
+++ SpaceshipClicker/SpaceshipClicker.Web/Models/AccountViewModels/LoginViewModel.cs
@@ -5,8 +5,9 @@
     public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
